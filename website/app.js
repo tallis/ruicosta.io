6b7f11166b5a93d6e81255a32f998b5b9de78168
Tallis <ruicosta@ieee.org>
2014-11-23 21:36:01 +0000

    Basic website funcionality

--- website/app.js
+++ website/app.js
@@ -1,13 +1,15 @@
 var express = require('express'),
     exphbs  = require('express-handlebars');
+var path = require('path');
 
 var app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.render('index');
 });
 
-app.listen(3000);
+app.listen(80);
